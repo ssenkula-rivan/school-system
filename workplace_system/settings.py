bdf2300d1c6cee6b374f56e36921d1ec14bc0b02
ssenkula-rivan <cranictech@gmail.com>
2026-02-25 15:49:28 +0300

    Add CSRF_TRUSTED_ORIGINS and set DEBUG to False

--- workplace_system/settings.py
+++ workplace_system/settings.py
@@ -28,9 +28,15 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-e1cpt8!-a3!paktt851hlp9rav6#b@3ms@3sby478@^5aolj-w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',')
+
+# CSRF trusted origins for Render
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.onrender.com',
+    'https://school-management-system-vipe.onrender.com',
+]
 
 
 # Application definition
